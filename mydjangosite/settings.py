af74d488a5627deb7d22ad0e53ab1a148224c651
LloydNicholson <nichlloyd@gmail.com>
2019-09-12 09:23:52 +0200

    Added Django Guitar Player image and changed some styles

--- mydjangosite/settings.py
+++ mydjangosite/settings.py
@@ -14,8 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -55,9 +53,7 @@ ROOT_URLCONF = 'mydjangosite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            TEMPLATE_DIR,
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,8 +113,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_FILES_DIRS = [
-    STATIC_DIR,
-]
-
-print(STATIC_DIR)
